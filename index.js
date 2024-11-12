0fc9025ce5303b44a957973ef259e2eeeade29d3
the-codingguy <kamarajraja2906@gmail.com>
2024-11-12 23:42:54 +0530

    Added Cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { resolve } = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const port = 3010;
 
 app.use(express.static('static'));
